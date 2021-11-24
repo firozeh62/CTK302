7099a89bfa16b1c8511c804947e9bdbc351e2900
firozeh62 <firozeh.art@gmail.com>
2021-11-24 14:31:52 -0600

    Gyro

--- Unit4/Gyro/sketch.js
+++ Unit4/Gyro/sketch.js
@@ -60,11 +60,7 @@ function draw() {
   // Text that makes CTK type in the background
   fill('white');
   noStroke();
-<<<<<<< HEAD
-  textSize(200);
-=======
-  textSize(300);
->>>>>>> 9d94d4eb5807e934b6cde9d520450f6ef8adedbf
+  textSize(100);
   textAlign(CENTER);
   text("Circus", width / 2, height / 2);
 
